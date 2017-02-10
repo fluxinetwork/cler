6ee4baffa99bc32e3e0ececb56ab3f910f7153de
Rolland Yann <rollandyann@gmail.com>
2017-02-10 09:55:05 +0100

    Map adhérents

--- app/inc/forms/manage-map-adherents.php
+++ app/inc/forms/manage-map-adherents.php
@@ -26,7 +26,8 @@ function get_json_map(){
 				'post_type' => $post_type,
 				'posts_per_page' => $posts_per_page,
 				'post_status' => $post_status,
-				'tag' => $tag				
+				'meta_key' => 'type_structure',
+				'meta_value' => $tag							
 			);
 		else :	
 			/*if($tag=='all_cat'):
