25c47e4cf34b252433a5ff3ed4e6ed1ea4466dff
Rolland Yann <rollandyann@gmail.com>
2017-02-14 18:35:52 +0100

    Allé events pagination

--- page-templates/page-tous-events.php
+++ page-templates/page-tous-events.php
@@ -166,16 +166,19 @@ Template Name: Toutes les événements
 				endwhile;
 
 			echo '</ul>';
-	            
+
 	        echo '<div class="pagination">';
-	    		echo paginate_links( array(
-	    			'base' => @add_query_arg('paged','%#%'),
-	    			'format' => '?paged=%#%',
-	    			'current' => max( 1, get_query_var('paged') ),
-	    			'total' => $query_filtered->max_num_pages,
-	          		'prev_next'=> false
-	    		) );
-	        echo '</div>';
+			    echo '<div class="nav-links">';
+			    echo paginate_links( array(
+			    	'base' => @add_query_arg('paged','%#%'),
+			    	'before_page_number' => 'Page ',
+			    	'format' => '?paged=%#%',
+			    	'current' => max( 1, get_query_var('paged') ),
+			    	'total' => $query_filtered->max_num_pages,
+			    	'prev_next'=> false
+			    ) );
+			    echo '</div>';
+		    echo '</div>';
 				
 		else :
 
