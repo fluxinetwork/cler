759e3261a57dfacdd31ce275688bc930e057e616
Thibaut Caroli <thibaut.caroli.pro@gmail.com>
2017-02-17 15:55:23 +0100

    Limit download item title

--- page-templates-parts/downloads.php
+++ page-templates-parts/downloads.php
@@ -11,7 +11,7 @@
 
 		while ( have_rows('onglet_documents') ) : the_row();			
 			
-			$texte_onglet = get_sub_field('texte_onglet');
+			$texte_onglet = limitString(get_sub_field('texte_onglet'), 0, 80, ' [...]');;
 			$documents = get_sub_field('documents');
 
 			$all_onglets .= '<a href="#" class="l-download__title js-tab" data-tab="'.$nb_tabs.'">'.$texte_onglet.'<span class="c-dash"></span></a>';
@@ -67,53 +67,3 @@
 		// No items
 	endif;
 ?>
-
-<?php /*
-<ul class="l-col list-reset l-grid">
-	<li class="l-download is-active l-grid__col">
-		<a href="#" class="l-download__title">Rapport financier 2015 <div class="c-dash"></div></a>
-		<ul class="l-download__list">
-			<li class="l-download__list__item">
-				<a href="https://cler.org/dev-cler/wp-content/uploads/2017/01/rapporttresorier_cler2015.pdf" class="c-downloadItem">
-					<i class="fa fa-download c-downloadItem__icon"></i>
-					<span class="c-downloadItem__title">Rapport du trésorier 2015</span>
-				</a>
-			</li>
-			<li class="l-download__list__item">
-				<a href="https://cler.org/dev-cler/wp-content/uploads/2017/01/rapporttresorier_cler2015.pdf" class="c-downloadItem">
-					<i class="fa fa-download c-downloadItem__icon"></i>
-					<span class="c-downloadItem__title">Rapport du trésorier 2015</span>
-				</a>
-			</li>
-			<li class="l-download__list__item">
-				<a href="https://cler.org/dev-cler/wp-content/uploads/2017/01/rapporttresorier_cler2015.pdf" class="c-downloadItem">
-					<i class="fa fa-download c-downloadItem__icon"></i>
-					<span class="c-downloadItem__title">Rapport du trésorier 2015</span>
-				</a>
-			</li>
-			<li class="l-download__list__item">
-				<a href="https://cler.org/dev-cler/wp-content/uploads/2017/01/rapporttresorier_cler2015.pdf" class="c-downloadItem">
-					<i class="fa fa-download c-downloadItem__icon"></i>
-					<span class="c-downloadItem__title">Rapport du trésorier 2015</span>
-				</a>
-			</li>
-			<li class="l-download__list__item">
-				<a href="https://cler.org/dev-cler/wp-content/uploads/2017/01/rapporttresorier_cler2015.pdf" class="c-downloadItem">
-					<i class="fa fa-download c-downloadItem__icon"></i>
-					<span class="c-downloadItem__title">Rapport du trésorier 2015</span>
-				</a>
-			</li>
-		</ul>
-	</li>
-	<li class="l-download l-grid__col">
-		<a href="#" class="l-download__title">Rapport d'activités 2015 <div class="c-dash"></div></a>
-		<ul class="l-download__list">
-			<li class="l-download__list__item">
-				<a href="https://cler.org/dev-cler/wp-content/uploads/2017/01/CLER-rapport2015.pdf" class="c-downloadItem">
-					<i class="fa fa-download c-downloadItem__icon"></i>
-					<span class="c-downloadItem__title">CLER-rapport2015</span>
-				</a>
-			</li>
-		</ul>
-	</li>
-</ul> */ ?>
