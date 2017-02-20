d4411730b3ced5247897f3e770349977f015e4ef
Thibaut Caroli <thibaut.caroli.pro@gmail.com>
2017-02-20 12:55:28 +0100

    Formateree footer

--- page-templates-parts/base/footer-formateree.php
+++ page-templates-parts/base/footer-formateree.php
@@ -1,4 +1,15 @@
 <footer class="l-row bg-dark footer" id="contact">
-	<div class="l-col footer__content l-grid">
+	<div class="l-col l-footer__content l-grid">
+		<div class="l-grid__col l-footer__content__col1">
+			<img src="<?php echo get_bloginfo('template_url'); ?>/app/img/formateree-logo-symbole-mini.png" alt="Symbole du logo de Format'eree - Formations de la transition énergétique">
+			<p class="t-meta t-meta--white l-grid__col">Format'eree<br>Formations de la<br>transition énergétique</p>
+		</div>
+		<div class="l-grid__col l-footer__content__col2">
+			<a href="https://twitter.com/Formateree" class="c-btnIcon c-btn--ghost"><i class="fa fa-twitter"></i></a>
+		</div>
+		<div class="l-grid__col l-footer__content__col3">
+			<p class="t-meta t-meta--white mgRight--m u-show@med">Une action menée<br>par le CLER - Réseau pour la<br>transition énergétique</p> 
+			<a href="<?php echo get_bloginfo('url'); ?>" class="c-btn c-btn--ghost" target="_blank"><i class="fa fa-share external-link mgRight--xs"></i>Le site du CLER</a>
+		</div>
 	</div>
 </footer>
