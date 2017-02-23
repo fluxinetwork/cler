43c093e1df486d56386606f9449971dc59c55db6
Rolland Yann <rollandyann@gmail.com>
2017-02-23 13:25:20 +0100

    Images in Rss feed

--- functions/rss.php
+++ functions/rss.php
@@ -12,27 +12,19 @@
 function fluxi_rss_custom($content) {
 	global $post;
 	$post_id = $post->ID;
-
-	// Content	
-	$main_image_obj = get_field( 'main_image', $post_id);
 	$news_img ='';
 	
-	if ( has_post_thumbnail($post_id) && empty($main_image_obj)) :
-		$post_img_id = get_post_thumbnail_id($post_id);
-		$post_img_array = wp_get_attachment_image_src($post_img_id, 'card--rss', true);
-		$post_img_url = $post_img_array[0];
-	
-		$news_img = '<img src="'.$post_img_url.'">';
-	elseif(!empty($main_image_obj)):
-	
-		$news_img = '<img src="'.$main_image_obj['sizes']['card--rss'].'">';
+	// Content	
+	if ( get_field('add_image', $post_id) == 1 ) :
+		$post_img_id = get_field('main_image', $post_id);
+		if($post_img_id):
+			$post_img_array = wp_get_attachment_image_src($post_img_id, 'card--rss', true);
+			$post_img_url = $post_img_array[0];
+			$news_img = '<img src="'.$post_img_url.'">';
+		endif;
+	endif;	
 
-	else:
-
-		$news_img = '';
-	endif;		
-
-	$content = $news_img . '<p>' . get_field('fluxi_resum', $post_id) .'</p>';
+	$content = $news_img . '' . get_field('fluxi_resum', $post_id) .'';
 
 	return $content;
 }
