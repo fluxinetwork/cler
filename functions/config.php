189c5f88a5860e6f214e7fd976fbe3f8aaaa4695
Thibaut Caroli <thibaut.caroli.pro@gmail.com>
2017-02-27 17:45:04 +0100

    Better formation dashboard

--- functions/config.php
+++ functions/config.php
@@ -16,9 +16,6 @@ define('POST_EXCERPT_LENGTH', 40);
 define('SLIDER_NB_POSTS', 10);
 define('LIMIT_STRING', 90);
 
-//define('STRIPE_KEY', 'sk_live_JdJRbJpPwMfPogZZ85Vja5NA');
-define('STRIPE_KEY', 'sk_test_v6Ap9E383Hth1I9JkIuHXJFJ');
-
 define('GOOGLE_ANALYTICS_ID', '');
 define('GOOGLE_MAP_API_KEY', 'AIzaSyCHOJtRqM6DSAm6HXYsYSITlHhzkvLKH_M');
 /* ACF Google Maps */
--- page-templates-parts/content-formation.php
+++ page-templates-parts/content-formation.php
@@ -16,17 +16,22 @@
 			</div>
 
 			<?php
-				$ville = get_field('adresse_formation');
-				$adresse = get_field('adresse');
-				$nom = get_field('nom_centre');
+				// ORGANISME
+				$nom_centre = get_field('nom_centre');
+				$ville_centre = get_field('ville');
+				$code_postal_centre = get_field('code_postal');
+				$adresse_centre = get_field('adresse');
+				$telephone_centre = get_field('telephone');
+				$mail_centre = get_field('contact_email');
 
-				$formateree = get_field('agrement_formateree');
-
+				//FORMATION
+				$ville_formation = get_field('adresse_formation');
 				$ob_departement = get_field_object('field_574dab093c7b0');
-				$label_departement = $ob_departement['choices'][ get_field('departement') ];
-				$code_postal = get_field('code_postal');
-				$numero_departement = substr($ob_departement['value'], 12, 3);
-				
+				$label_departement_formation = $ob_departement['choices'][ get_field('departement') ];
+				$numero_departement_formation = substr($ob_departement['value'], 12, 3);
+				$duree_formation = get_field('type_duree_formation');
+				$duree_formation = ($duree_formation=='longue') ? 'Formation longue' : 'Formation courte';
+				$formateree = get_field('agrement_formateree');
 				$link_formation = get_field( 'site_internet' );
 			?>
 
@@ -34,17 +39,24 @@
 
 				<div class="l-miniDashboard__duo">
 					<div class="l-miniDashboard__duo__left">
-						<span class="t-meta"><i class="fa fa-id-card-o c-meta__meta__icon" aria-hidden="true"></i><?php echo $nom; ?></span>
-						<h3 class="c-card__body__title l-miniDashboard__duo__left__title"><?php echo $adresse; ?></h3>
+						<span class="t-meta mgBottom--xs">La formation</span>
+						<div class="l-miniDashboard__duo__left__title">
+							<h3 class="c-card__body__title"><?php echo $ville_formation; ?></h3>
+							<h4 class="h5"><?php echo $label_departement_formation; ?></h4>
+							<span class="t-meta"><i class="fa fa-arrows-h mgRight--xs"></i><?php echo $duree_formation; ?></span>
+						</div>
+						
 						<?php if ($link_formation) : ?>
 							<a href="<?php echo $link_formation; ?>" class="c-link c-link--more l-miniDashboard__duo__left__link" target="_blank">Site internet de la formation</a>
 						<?php endif; ?>
 					</div>
-					<div class="l-miniDashboard__duo__right">
-						<div class="l-miniDashboard__duo__right__nb"><?php echo $numero_departement; ?></div>
-						<div class="l-miniDashboard__duo__right__txt">
-							<div class="t-meta t-meta--dark"><i class="fa fa-map-marker c-meta__meta__icon" aria-hidden="true"></i><?php echo $ville; ?></div>
-							<div class="t-meta t-meta--dark mgTop--xs"><i class="fa fa-location-arrow c-meta__meta__icon" aria-hidden="true"></i><?php echo $label_departement; ?></div>
+					<div class="l-miniDashboard__duo__right l-miniDashboard__duo__right--formation"> 
+						<span class="t-meta mgBottom--xs">L'organisme de formation</span>
+						<div class="l-miniDashboard__duo__left__title">
+							<h3 class="c-card__body__title"><?php echo $nom_centre; ?></h3>
+							<h4 class="h5"><?php echo $adresse_centre.'<br>'.$code_postal_centre.', '.$ville_centre; ?></h4>
+							<span class="t-meta "><i class="fa fa-envelope mgRight--xs"></i><?php echo $mail_centre; ?></span><br>	
+							<span class="t-meta"><i class="fa fa-phone mgRight--xs"></i><?php echo $telephone_centre; ?></span>
 						</div>
 					</div>
 				</div>
